9fd5f60a00165b111fd5030b3959f2488ce11d06
Jose Chavira <joeychavira2016@gmail.com>
2019-09-03 00:01:51 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -27,7 +27,7 @@ def consolidate_cart(cart)
 	cart.each {|item_hash|
 		item_hash.each { |name, price_hash|
 			if hash[name].nil?
-				hash[name] = price_hash.merge({count: 1})
+				hash[name] = price_hash.merge({:count => 1})
 			else
 				hash[name][:count] += 1
 			end
